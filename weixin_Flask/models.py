2b54e992f6a8d5ebc40f66fa5de43af753255bb9
refer <262987680@qq.com>
2018-04-12 09:47:07 +0800

    1111

--- weixin_Flask/models.py
+++ weixin_Flask/models.py
@@ -1,4 +1,4 @@
-import datetime
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String, Text, ForeignKey, DateTime, UniqueConstraint, Index
