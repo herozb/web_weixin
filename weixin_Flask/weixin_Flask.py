804c63cd8cd405cbae30a6be90534ff9c491bad6
refer <262987680@qq.com>
2018-04-12 15:53:19 +0800

    添加前端代码

--- weixin_Flask/weixin_Flask.py
+++ weixin_Flask/weixin_Flask.py
@@ -1,12 +1,38 @@
-from flask import Flask
-
+from flask import Flask,render_template,request
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+#@app.route("/")
+#def index():
+#    return render_template("login.html")
+
+function_list = {
+    1:{'action':'查看用户信息'},
+    2:{'action':'查看公司信息'},
+    3:{'action':'添加用户信息'},
+    4:{'action':'修改用户信息'},
+    5:{'action':'添加公司信息'},
+    6:{'action':'修改公司信息'}
+}
+
+
+@app.route("/index",methods = ['GET', 'POST'])
+def index():
+    return render_template('index.html',user_dict=function_list)
+
+@app.route("/login",methods = ['GET', 'POST'])
+def login():
+    if request.method == "GET":
+        return render_template('login.html')
+    else:
+        user = request.form.get('user')
+        pwd = request.form.get('pwd')
+        if user=="zhangbing" and pwd=="123":
+            return render_template('index.html')
+        else:
+            return "<h1>login Failure !</h1>"
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
